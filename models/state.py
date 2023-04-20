b3d979607693f91d42c6320b3d2f6c867a428140
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-20 09:47:44 +0100

    Revert "updated users"

--- models/state.py
+++ models/state.py
@@ -1,28 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
-
-#    name = ""
-
-    @property
-    def cities(self):
-        """Getter attribute that returns the list of City instances with state_id
-        equals to the current State.id."""
-        from models import storage
-        from models.city import City
-
-        cities = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities.append(city)
-        return cities
+    name = ""
