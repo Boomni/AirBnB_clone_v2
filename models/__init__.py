cb1c070b55f291d4776e7c9b0c245bfde59638f7
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-04-20 04:19:36 +0100

    updated users

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+import os
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 
-storage = FileStorage()
-storage.reload()
+#storage = FileStorage()
+#storage.reload()
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+    storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
+#    name = ""
+
+    @property
+    def cities(self):
+        """Getter attribute that returns the list of City instances with state_id
+        equals to the current State.id."""
+        from models import storage
+        from models.city import City
+
+        cities = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                cities.append(city)
+        return cities
