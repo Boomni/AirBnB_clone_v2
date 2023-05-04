506b43e0fccd640310c4b14a2ff446effc4c516f
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-04 16:34:57 +0100

    Wrote a Fabric script that generates a .tgz archive from the contents of the web_static folder

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,6 +26,4 @@ sudo chown -hR ubuntu:ubuntu /data/
 
 sudo sed -i 's#server_name _;#server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n#g' /etc/nginx/sites-available/default
 
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-
 sudo service nginx restart
