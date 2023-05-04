fd8d0611397b41d3852a11c38f31eac6aeb9e3bf
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-04 16:19:28 +0100

    Wrote a Bash script that sets up my web servers for the deployment of web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Fabric Script"""
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """Create a compressed archive of the web_static folder."""
+    local("mkdir -p versions")
+    
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_" + now + ".tgz"
+    
+    # create the archive
+    result = local("tar -czvf versions/{} web_static".format(archive_name))
+    
+    if result.failed:
+        return None
+    else:
+        return "versions/{}".format(archive_name)
