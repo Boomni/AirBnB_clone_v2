4f3b39219e9b26c9e910c21a92676306e9fb0895
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-07 18:06:28 +0100

    archive creation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,16 +7,12 @@ execute: fab -f 1-pack_web_static.py do_pack
 from datetime import datetime
 from fabric.api import *
 
-
 def do_pack():
-    """
-    making an archive on web_static folder
-    """
-
-    time = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + time + '.tgz'
-    local('mkdir -p versions')
-    create = local(f'tar -cvzf {archive_path} web_static/')
-    if create:
-        return archive_path
-    return None
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time))
+        return ("versions/web_static_{}.tgz".format(time))
+    except:
+        return None
