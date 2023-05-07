0cc88e573b7bafc3a47aedecf134d4f63f78cd0c
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-07 18:38:37 +0100

    documented my function properly

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
 """
 
 from os.path import exists
-from fabric.api import put, run, env
+from fabric.api import run, put, local, sudo
 
 env.hosts = ['54.237.112.44', '35.175.63.68']
 
@@ -19,13 +19,15 @@ def do_deploy(archive_path):
     if not exists(archive_path):
         return False
     try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        put('archive_path', '/tmp')
+        put(archive_path, '/tmp/')
+        file_name = archive_path.split('/')[-1].split('.')[0]
         run(f"mkdir -p /data/web_static/releases/{file_name}")
-        run(f"tar -xzvf /tmp/{file_name}.tgz -C /data/web_static/releases/{file_name}/")
-        run(f'rm -rf /tmp/{file_name}.tgz')
+        run(f"tar -xzf /tmp/{file_name}.tgz -C /data/web_static/releases/{file_name}/")
+        run(f'rm /tmp/{file_name}.tgz')
+        run(f'mv /data/web_static/releases/{file_name}/web_static/* /data/web_static/releases/{file_name}')
+        run('rm -rf /data/web_static/releases/{file_name}/web_static')
         run('rm -rf /data/web_static/current')
-        run(f'ln -sf /data/web_static/releases/{file_name} /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{file_name}/ /data/web_static/current')
         print('New version deployed!')
         return True
     except Exception:
