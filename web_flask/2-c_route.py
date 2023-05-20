dfc8ead6321094ade4baaa7d7cb51fd6e3075923
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-20 22:32:35 +0100

    Debugged error

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-"""Flask web application"""
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+"""
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return 'Hello HBNB!'
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
-    return 'HBNB'
+    """prints HBNB"""
+    return "HBNB"
 
 
-@app.route("/c/<string:text>", strict_slashes=False)
+@app.route('/c/<string:text>', strict_slashes=False)
 def c_text(text):
-    """
-    Displays "C " followed by the value of the text variable
-    Replaces underscore _ symbols with a space
-    """
+    """prints C followed by <text> content"""
     text = text.replace("_", " ")
-    return f'C {text}'
+    return "C %s" % text
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
