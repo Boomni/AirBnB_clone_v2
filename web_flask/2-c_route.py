0750bf8424955474a87b05556c22818f82c2e86c
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-20 22:18:14 +0100

    Debugged error

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,8 +21,8 @@ def hbnb():
 @app.route("/c/<string:text>", strict_slashes=False)
 def c_text(text):
     """
-    Display “C” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
+    Displays "C " followed by the value of the text variable
+    Replaces underscore _ symbols with a space
     """
     text = text.replace("_", " ")
     return f"C {text}"
