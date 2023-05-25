28488d078b7bf0e463e8f01f4ed7419577eba309
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-25 10:32:34 +0100

    Minor changes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,17 +15,18 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
 @app.teardown_appcontext
-def teardown_session(exception=None):
-    """Remove the current SQLAlchemy session"""
-    storage.close()
+def teardown_db(exception):
+    """ teardown db"""
+    if storage is not None:
+        storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ list of state ids"""
+    data = storage.all(State)
+    return render_template('7-states_list.html', total=data.values())
 
 
 if __name__ == '__main__':
